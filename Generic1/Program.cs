7c4190f0224a958f5336182ec31028cb5a4a5dfb
Vusalmb <vusalmb@code.edu.az>
2021-10-12 09:45:38 +0400

    Добавьте файлы проекта.

---
+++ Generic1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Generic1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Programming programm1 = new Programming("C#", 2002);
+            Programming programm2 = new Programming("HTML", 1990);
+            Programming programm3 = new Programming("Javascipt", 1995);
+
+            Collections<Programming> collection = new Collections<Programming>();
+
+            collection.Add(programm1);
+            collection.Add(programm2);
+            collection.Add(programm3);
+
+            Console.WriteLine(collection.Count);
+            Console.WriteLine("Programming: ");
+
+            foreach (var item in collection.GetArray())
+            {
+                Console.WriteLine(item.Name);
+                Console.WriteLine(item.Year);
+            }
+        }
+    }
+}
---
+++ Generic1/Programming.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generic1
+{
+    class Programming
+    {
+        public string Name;
+        public int Year;
+
+        public Programming(string name, int year)
+        {
+            Name = name;
+            Year = year;
+        }
+    }
+}
